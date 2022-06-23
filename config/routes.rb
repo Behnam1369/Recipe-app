197c688661984d1e9e0216b41724d7a6f34e2cdb
lisandroseia <lisandroseia11@gmail.com>
2022-06-23 11:51:54 -0300

    Add recipy food list

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :foods
   devise_for :users
   resources :users
-  resources :recipies, only: %i[index show new create destroy]
+  resources :recipies, only: %i[index show new create destroy] do
+    resources :recipy_foods, only: %i[create destroy new]
+  end
 
   root to: 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
