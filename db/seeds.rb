9a239bc400d142189dd3faa1e79216aa70014b54
lisandroseia <lisandroseia11@gmail.com>
2022-06-24 15:38:02 -0300

    Add request tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'lily@gmail.com', password: '123456', confirmed_at: DateTime.now, name: 'lily')
