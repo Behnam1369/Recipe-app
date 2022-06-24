a56d32bc4891190acea6896df5eef94862c75e96
lisandroseia <lisandroseia11@gmail.com>
2022-06-24 14:26:16 -0300

    Fix bug for recipies

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -23,11 +23,11 @@ class RecipiesController < ApplicationController
       format.html do
         if recipy.save
           flash[:success] = 'Recipy created!'
+          redirect_to action: :index
         else
           render :new
           flash[:error] = 'Something went wrong :('
         end
-        redirect_to action: :index
       end
     end
   end
@@ -43,8 +43,8 @@ class RecipiesController < ApplicationController
 
   def recipy_params
     response = params.require(:recipy).permit(:name, :preparation_time, :cooking_time, :description, :public)
-    response[:preparation_time] = response[:preparation_time].to_i
-    response[:cooking_time] = response[:cooking_time].to_i
+    response[:preparation_time] = response[:preparation_time]
+    response[:cooking_time] = response[:cooking_time]
     response
   end
 end
